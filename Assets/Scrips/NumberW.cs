14caeabb722ad4b8aadf8dd97684094475ddc286
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-03 06:57:02 +0100

    Refactor code

--- Assets/Scrips/NumberW.cs
+++ Assets/Scrips/NumberW.cs
@@ -9,6 +9,12 @@ public class NumberW : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		ShowInitialDebugLog ();
+
+		max += 1; // Prevent that the number 1000 won't be displayed
+	}
+
+	void ShowInitialDebugLog () {
 		Debug.Log ("Welcome to Number Wizard");
 		Debug.Log ("Pick a number in your head, but don't tell me!");
 
@@ -17,24 +23,24 @@ public class NumberW : MonoBehaviour {
 
 		Debug.Log ("Is the number higher or lower than " + guess + "?");
 		Debug.Log ("Up = higher, down = lower, return = equal");
-
-		max += 1; // Prevent that the number 1000 won't be displayed
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) { // Up arrow key pressed
 			min = guess;
-			guess = (max + min) / 2;
-
-			Debug.Log ("Higher or lower than " + guess + "?");
+			NextGuess ();
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) { // Down arrow key pressed
 			max = guess;
-			guess = (max + min) / 2;
-
-			Debug.Log ("Higher or lower than " + guess + "?");
+			NextGuess ();
 		} else if (Input.GetKeyDown (KeyCode.Return)) { // Enter key pressed
 			Debug.Log ("Your number is " + guess + "!!");
 		}
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		Debug.Log ("Higher or lower than " + guess + "?");
+		Debug.Log ("Up = higher, down = lower, return = equal");
 	}
 }
