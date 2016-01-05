50dc2f64e783e66d4d2afb762be149ad93e0dfa5
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-05 11:03:58 +0100

    Finished game UI and logic

--- Assets/Scrips/NumberW.cs
+++ Assets/Scrips/NumberW.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class NumberW : MonoBehaviour {
@@ -6,6 +8,10 @@ public class NumberW : MonoBehaviour {
 	int max;
 	int min;
 	int guess;
+
+	int maxGuessesAllowed = 10;
+
+	public Text text;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +30,13 @@ public class NumberW : MonoBehaviour {
 		Debug.Log ("Is the number higher or lower than " + guess + "?");
 		Debug.Log ("Up = higher, down = lower, return = equal");
 
+		//text.text = "Welcome to Number Wizard\n" +
+		//	"Pick a number in your head, but don't tell me!\n" +
+		//	"The highest number you can pick is " + max + "\n" +
+		//	"The lowest number you can pick is " + min + "\n" +
+		//	"Is the number higher or lower than " + guess + "?\n" +
+		//	"Up = higher, down = lower, return = equal";
+
 		max += 1; // Prevent that the number 1000 won't be displayed
 	}
 
@@ -32,24 +45,30 @@ public class NumberW : MonoBehaviour {
 		min = 1;
 		guess = 500;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow)) { // Up arrow key pressed
-			min = guess;
-			NextGuess ();
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) { // Down arrow key pressed
-			max = guess;
-			NextGuess ();
-		} else if (Input.GetKeyDown (KeyCode.Return)) { // Enter key pressed
-			Debug.Log ("Your number is " + guess + "!!");
-			InitiliceVariables ();
-			ShowInitialDebugLog ();
-		}
+
+	public void GuessLower () {
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher () {
+		min = guess;
+		NextGuess ();
 	}
 
 	void NextGuess () {
 		guess = (max + min) / 2;
+		maxGuessesAllowed -= 1;
+
+		text.text = guess.ToString ();
+
+			//"Higher or lower than " + guess + "?\n" +
+			//"Up = higher, down = lower, return = equal";
+
+		if (maxGuessesAllowed <= 0) {
+			SceneManager.LoadScene ("Win");
+		}
+
 		Debug.Log ("Higher or lower than " + guess + "?");
 		Debug.Log ("Up = higher, down = lower, return = equal");
 	}
